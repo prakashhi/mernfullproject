f81ffdda93574e7d7e6ead5d2bf5efea4c0c7801
Prakash <prakash398prajapati@gmail.com>
2024-11-07 10:59:42 +0530

    test06

--- src/Components/Camera.jsx
+++ src/Components/Camera.jsx
@@ -78,11 +78,13 @@ const Camera = () => {
         const mostLikelyExpression = getMostLikelyExpression(detectedExpressions);
 
         // Check if the detected expression matches the expected one
-        if (mostLikelyExpression === expectedExpression) {
-          setExpressionMatched(true); // Set match as true to stop expression updates
-        } else {
-          setExpressionMatched(false);
+        if (expressionMatched(false))
+        {
+          if (mostLikelyExpression === expectedExpression) {
+            setExpressionMatched(true); // Set match as true to stop expression updates
+          }
         }
+
       } else {
         setFaceDetected(false);
         setDetectionAccuracy(0);
@@ -127,7 +129,7 @@ const Camera = () => {
 
   return (
     <div className='bg-gradient-to-r from-slate-500 to-slate-800 inline-grid justify-center p-2 relative rounded mb-3'>
-      <div className='relative w-full mb-3'>
+      <div className='relative w-full mb-3 max-[450px]:h-[80%]'>
         <Webcam className='w-full h-full rounded' ref={webcamRef} />
         <canvas className='absolute top-0 left-0 w-full h-full' ref={canvasRef} />
       </div>
