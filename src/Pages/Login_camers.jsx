640f1847b3ac48f9f3ca57b309c2cc8a3f216ed2
Prakash <prakash398prajapati@gmail.com>
2024-12-04 21:17:24 +0530

    dsfDS

--- src/Pages/Login_camers.jsx
+++ src/Pages/Login_camers.jsx
@@ -148,8 +148,14 @@ const Login_camers = () => {
 
         // Check if the detected expression matches the expected one
         if (mostLikelyExpression === expectedExpression) {
-			setload(false);
-		    expressionMatched.current = true;
+			   if (!expressionMatched.current) {
+					setload(false);
+					expressionMatched.current = true;
+					setIsExpressionMatched(true); // Update state for UI
+				}
+			
+			//setload(false);
+		   // expressionMatched.current = true;
 			
          // if (!expressionMatched) { // Only set to true if it hasn’t already been set
 				
@@ -243,7 +249,7 @@ const Login_camers = () => {
             {/* Display match status */}
             <div className='bg-white text-gray-700 p-3 rounded mt-3'>
               <h3 className="font-bold">Expression Match Status:</h3>
-              <p>{expressionMatched == true ? "Matched!" : "Not Matched"}</p>
+              <p>{expressionMatched.current ? "Matched!" : "Not Matched"}</p>
 
             </div>
 
@@ -254,7 +260,7 @@ const Login_camers = () => {
             <p>{textualAnalysis}</p>
           </div>
 
-          {expressionMatched == true ? <button onClick={saveFaceEncoding} className='hover:px-9 hover:py-3 duration-[0.5s] text-white bg-fuchsia-600 rounded-full font-extrabold px-7 py-2'>
+          {expressionMatched.current ? <button onClick={saveFaceEncoding} className='hover:px-9 hover:py-3 duration-[0.5s] text-white bg-fuchsia-600 rounded-full font-extrabold px-7 py-2'>
             {
               load == true ? (<svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
